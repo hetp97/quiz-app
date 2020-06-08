bce3dc80a82ae778f2e02c9d5b904816b9782c70
Het Parekh <45689722+hetp97@users.noreply.github.com>
2020-06-08 22:46:42 +0530

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,44 @@
+const correctAnswers= ['C','A','A','B','C'];
+const form = document.querySelector('.quiz-form');
+const result= document.querySelector('.result');
+
+form.addEventListener('submit', e=>
+{
+	e.preventDefault();
+
+
+let score= 0;
+const userAnswers =[form.q1.value, form.q2.value, form.q3.value, form.q4.value, form.q5.value];
+
+//check answers
+
+
+userAnswers.forEach((answer, index)=>
+{
+	if(answer===correctAnswers[index]){
+		score+=20;
+
+}
+
+})
+//console.log(score);
+//check result
+window.scrollTo(0,0);
+//result.querySelector('span').textContent= `${score}%`;
+result.classList.remove('d-none');
+
+let output=0;
+const timer = setInterval(()=>{
+result.querySelector('span').textContent= `${output}%`;
+if(output===score)
+{
+	clearInterval(timer);
+
+}
+else{
+	output++;
+}
+
+},10);
+
+});
